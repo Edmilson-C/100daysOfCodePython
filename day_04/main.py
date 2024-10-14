2f28b343228eb60734d7090a92826a182b638026
Edmilson-C <edmilson.conceicao09@gmail.com>
2024-10-14 20:38:30 +0200

    feat(day 04): add input validation

--- day_04/main.py
+++ day_04/main.py
@@ -10,15 +10,18 @@ print("####################################################")
 options = ["Rock", "Paper", "Scissors"]
 
 user = int(input("Type 0 - Rock, 1 - Paper, 2 - Scissors:\n"))
-print(options[user])
 
-machine = random.randint(0, 2)
-print(f"Computer chose {options[machine]}")
+if user >= 3 or user < 0:
+    print("Wrong option!")
+else:
+    print(options[user])
+    machine = random.randint(0, 2)
+    print(f"Computer chose {options[machine]}")
 
-if user == machine:
-    print("It's a draw")
-elif (user == 0 and machine == 1) or (user == 1 and machine == 2) or (user == 2 and machine == 0):
-    print("You lose")
-elif (user == 0 and machine == 2) or (user == 1 and machine == 0) or (user == 2 and machine == 1):
-    print("You win")
+    if user == machine:
+        print("It's a draw")
+    elif (user == 0 and machine == 1) or (user == 1 and machine == 2) or (user == 2 and machine == 0):
+        print("You lose")
+    elif (user == 0 and machine == 2) or (user == 1 and machine == 0) or (user == 2 and machine == 1):
+        print("You win")
 
